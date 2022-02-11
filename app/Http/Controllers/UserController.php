2d0b17f5e2d49b0b4614a04f3b61343cb6e7106a
knammert <knammert@gmail.com>
2022-02-11 14:40:04 +0100

    Edit profile fixes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,8 +15,6 @@ class UserController extends Controller
     {
         $users = User::latest()->paginate(10);
 
-
-
         // return view('adminPanel.users.index',compact('users','xd'))
         //      ->with('i', (request()->input('page', 1) - 1) * 5);
         return view('adminPanel.users.index', ['users' => $users])
