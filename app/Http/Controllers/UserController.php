0394f27abe48a955ce622dc970f015cca07a9107
knammert <knammert@gmail.com>
2022-05-02 15:33:24 +0200

    Small Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,6 +19,15 @@ class UserController extends Controller
         $classes = Class_name::all();
         $users   = User::usersByRoleOrName()
         ->paginate(10);
+        $included = get_included_files();
+        print_r($included);
+
+        function convert($size)
+        {
+            $unit=array('b','kb','mb','gb','tb','pb');
+            return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.$unit[$i];
+        }
+        echo convert(memory_get_usage(false)); 
 
         return view('adminPanel.users.index', ['users' => $users, 'classes'=> $classes])
             ->with('i', (request()->input('page', 1) - 1) * 5);;
